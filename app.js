3a2240d66be7f911956380655423a88e26bd510f
Inton1701 <aristonmagboo17@gmail.com>
2024-10-02 00:39:03 +0800

    final

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', routes);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3000)
